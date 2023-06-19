0bc0e62c497a48df97c4a8e39106a1faafc5acd4
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2023-06-19 13:03:04 -0600

    primeras lineas de api en node

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Curso de Node Js
+
+- Iniciando proyecto, aceptando la configuración por default
+```
+npm init --y
+```
+
+- Inciando proyecto con pasos personalizados
+```
+npm init
+```
+
+- Intalar dependencias en el proyecto
+- Se intalan las dependencias dentro del archivo package.json en el apartado de dependecies
+```
+npm install paquete
+npm install paquete --save
+
+// forma corta
+
+npm i paquete paquete
+npm i paquete paquete -S
+
+```
+
---
+++ curso_node_api_rest/app.js
@@ -0,0 +1,24 @@
+// importamos los paquetes que se requieren
+const expres = require("express")
+const cors = require("cors")
+
+// cargamos las variables de entorno
+require("dotenv").config()
+
+// creamos la instancia del servidor
+const app = expres()
+
+// se especifica el puesto en el cual se levanta el servidor
+// const port = 3000; 
+// obtenemos el puerto de las variables de entorno
+const port = process.env.PORT || 3000
+
+// utilizamos los cors para poder tener accedos desde donde sea
+app.use(cors())
+
+
+// corremos el servidor
+app.listen(port, () => {
+	// mosttramos un mensaje en consola si el servidor se inicio correctamente
+	console.log(`Servidor escuchando por el puesto ${port}`)
+})
