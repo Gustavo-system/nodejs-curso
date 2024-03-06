09bd0bdbb173c1e6b81849df4c2225f8d9ebf564
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-03-06 12:46:55 -0600

    se termina la demo de la documentacion del api

--- curso_node_api_rest/routes/users.js
+++ curso_node_api_rest/routes/users.js
@@ -8,6 +8,30 @@ const router = express.Router()
 
 // asignamos un controlador a la ruta
 router.get("/", validateTokenExist, validateAdmiRol(["admin"]), getUsers)
+/**
+ * @openapi
+ * /users/{id}:
+ *   get:
+ *     tags:
+ *       - user
+ *     summary: "Consultar usuario por id"
+ *     description: "Consultar la informacion de un usuario por medio de su id"
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: "Indicar el id del usuario que desee consultar"
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "1"
+ *     responses:
+ *       '200':
+ *         description: "Datos obtenidos con exito"
+ *       '404':
+ *         description: "El usuario no esta registrado"
+ */
 router.get("/:id", getOneUserValidator, getUser)
 router.post("/", validateTokenExist, createUserValidator, createUsers)
 router.put("/:id", getOneUserValidator, createUserValidator, updateUser)
